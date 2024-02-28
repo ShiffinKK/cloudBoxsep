9023ab9b151c20aff0d298ad643ad15f7358ab96
Shiffin <shiffinkurian@gmail.com>
2024-02-28 09:45:26 +0530

    initial commit

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from store.models import Category,Product,Size
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Size)
 
 # Register your models here.
